b5bef51fcdf4e31b42089003cc0f4e9412828a13
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2023-09-27 17:43:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {
   onAuthStateChanged,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { timetables } from "./timetable.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -72,33 +73,7 @@ function changeSize() {
 changeSize();
 //------------
 //notify
-const timetables = {
-  manju: {
-    subject: "physics",
-    monday: ["", "12A", "", "", "12A", "12A", "", ""],
-    tuesday: ["", "", "", "12A", "12A", "", "12A", ""],
-    wednesday: ["", "11A", "11A", "", "12A", "", "", ""],
-    thursday: ["", "11A", "11A", "", "", "12A", "11A", ""],
-    friday: ["", "12A", "", ""],
-  },
-};
-/*
-timing
-1 - 8:15
-2 - 8:55
-3 - 9:35
-4 - 10:30
-5 - 11:10
-6 - 12:05
-7 - 12:45
-8 - 13:25
 
-fri
-1 - 8:20
-2 - 9:05
-3 - 10:05
-4 - 10:50
-*/
 const days = [
   "sunday",
   "monday",
