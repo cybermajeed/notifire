03fe21b83370a83407fb8641bde1b5778e03fd76
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2024-04-18 15:21:25 +0400

    Add files via upload

--- mobile/app/notifier.js
+++ mobile/app/notifier.js
@@ -1,1 +1,14 @@
 //getTimetableJSON(username), setInterval() -> backgroundNotification(periodNum)
+//https://dev.to/medaimane/background-processing-in-react-native-exploring-techniques-for-efficient-task-handling-2cbf
+//https://www.linkedin.com/pulse/how-use-local-notifications-react-native-waqas-khan
+
+/*
+{
+    subject: "Physics",
+    monday: ["", "12A", "", "", "12A", "12A", "", ""],
+    tuesday: ["", "", "", "12A", "12A", "", "12A", ""],
+    wednesday: ["", "11A", "11A", "", "12A", "", "", ""],
+    thursday: ["", "11A", "11A", "", "", "12A", "11A", ""],
+    friday: ["", "12A", "", ""],
+}
+*/
